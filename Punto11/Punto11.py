933d47baa75213990a2c556b35a83e926c56c395
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2024-08-10 18:21:41 -0300

    5to commit

--- Punto11/Punto11.py
+++ Punto11/Punto11.py
@@ -1,4 +1,6 @@
+from nis import match
 import random
+
 
 def numeroaleatorios():
     num1 = 0
@@ -7,4 +9,16 @@ def numeroaleatorios():
         num1 = random.randint(1,10)
         num2 = random.randint(1,10)
     return num1,num2
+
+jugar = True
+while jugar:
+    num1,num2 = numeroaleatorios()
+    eleccion = int(input("Elija el numero 1 o 2: "))
+    match eleccion:
+        case 1:
+            if num1 < num2:
+                print("Perdiste, numero 1: ",num1," numero 2: ",num2)
+                
+            
+
 
