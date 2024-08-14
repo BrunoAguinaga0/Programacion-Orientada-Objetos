8c3115bea5cef1f6a69a3db6ebb964f03eaec15e
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2024-08-14 17:16:06 -0300

    11vo Commit

--- TP2/Punto5-6-7/Punto5.py
+++ TP2/Punto5-6-7/Punto5.py
@@ -3,10 +3,10 @@ from Persona import Persona
 
 persona1 = Persona("Bruno","Aguinaga",'28/10/2001')
 print(persona1)
-
+print(f'Edad: {persona1.calculoEdad()}')
 persona2 = Persona(None,None,'10/11/2002')
 persona2.setNombre("Alan")
 persona2.setApellido("Viera")
 persona2.setFecha('24/07/2002')
 print(persona2)
-print(persona2.calculoEdad())
+print(f'Edad: {persona2.calculoEdad()}')
